731361a072944373cc04299ae259e0c908bb8d64
Dzmitry Lapitski <dz.lap.pro@gmail.com>
2023-03-22 12:40:06 +0100

    Added Lesson #3 new files. One slight change for one of the previous files.

--- Chapter #1 (Знакомимся с Selenium)/Lesson #6 (Поиск элементов с помощью Selenium WebDriver)/Step #11 (Уникальность селекторов - задание).py
+++ Chapter #1 (Знакомимся с Selenium)/Lesson #6 (Поиск элементов с помощью Selenium WebDriver)/Step #11 (Уникальность селекторов - задание).py
@@ -5,7 +5,7 @@ import time
 try:
     # Используй http://suninjuly.github.io/registration1.html для теста №1
     # и http://suninjuly.github.io/registration2.html для теста №2
-    link = "http://suninjuly.github.io/registration2.html"
+    link = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
---
+++ Chapter #3 (Тестовые фреймворки)/Lesson #2 (Тестирование web-приложений и тестовые фреймворки)/Step #13 (Оформляем тесты в стиле unittest).py
@@ -0,0 +1,90 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestUniqueSelectors(unittest.TestCase):
+
+    def test_unique_selectors_1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        first_name = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control first"][required]')
+        first_name.send_keys('Mike')
+        last_name = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control second"][required]')
+        last_name.send_keys('Smith')
+        email = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control third"][required]')
+        email.send_keys('example@mail.com')
+        phone = browser.find_element(By.CSS_SELECTOR, 'input:not(input[required])[class="form-control first"]')
+        phone.send_keys('8 800 555 35 35')
+        address = browser.find_element(By.CSS_SELECTOR, 'input:not(input[required])[class="form-control second"]')
+        address.send_keys('NY Main Street 255/55, 243')
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью self.assertEqual проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Welcome text is not correct or does not present.")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(2)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
+    def test_unique_selectors_2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        first_name = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control first"][required]')
+        first_name.send_keys('Mike')
+        last_name = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control second"][required]')
+        last_name.send_keys('Smith')
+        email = browser.find_element(By.CSS_SELECTOR, 'input[class="form-control third"][required]')
+        email.send_keys('example@mail.com')
+        phone = browser.find_element(By.CSS_SELECTOR, 'input:not(input[required])[class="form-control first"]')
+        phone.send_keys('8 800 555 35 35')
+        address = browser.find_element(By.CSS_SELECTOR, 'input:not(input[required])[class="form-control second"]')
+        address.send_keys('NY Main Street 255/55, 243')
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью self.assertEqual проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Welcome text is not correct or does not present.")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(2)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
